3653ac9a5656f685cef290f357e3fd6ffb98fef2
Henzii <80477931+Henzii@users.noreply.github.com>
2021-07-26 15:23:42 +0300

    dfsfds

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ const parse = require('./testi');
 app.get('/api/', async (req, res) => {
     res.json( await parse() )
 })
+const PORT = process.env.PORT || 3000
+console.log('Portissa' + PORT)
+app.listen(PORT)
 
-app.listen(3000);
