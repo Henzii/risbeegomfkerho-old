ac6b1946fd4df9c711436e6d0652148b4addbbcb
Henzii <80477931+Henzii@users.noreply.github.com>
2021-07-29 12:55:12 +0300

    f

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ app.use(cors())
 
 app.use( express.static('build'))
 
-app.get('/api/', async (req, res) => {
-    res.json( await parse() )
+app.get('/api/games', async (req, res) => {
+    const { games } = await parse()
+    res.json( games )
+})
+app.get('/api/courses', async( req, res) => {
+    const { courses } = await parse()
+    res.json( courses )
 })
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 console.log('Portissa' + PORT)
 app.listen(PORT)
 
--- testi.js
+++ testi.js
@@ -6,14 +6,7 @@ const parse = async () => {
     const rawData = await fs.readFile('score.csv', 'utf-8')
     const data = rawData.split('\n').reverse();
 
-    const courses = {
-        Malminiitty: { layout: 'Niitty', games: 0, players: {} },
-        Siltamäki: { layout: 'Updated', games: 0, players: {} },
-        'Lentolan Frisbeegolf': { games: 0, players: {} },
-        'Separi Simppari': { games: 0, players: {} },
-        'Separisimppa': { games: 0, players: {} },
-        'Ford DiscGolfPark': { layout: 'Blue layout', games: 0, players: {} }
-    }
+    const courses = []
     const players = {
         Henkka: {},
         Antti: {},
@@ -34,18 +27,23 @@ const parse = async () => {
         if (!(name in players) || !date.startsWith('2021')) {
             continue;
         }
-        if (!(courseName in courses) && (courses[courseName]?.layout === undefined || courses[courseName].layout === layout)) {
-            continue;
-        }
         if (date !== currentGame.course.date) {
             if (currentGame.players.length >= 4) {
+
+                let courseStats = courses.find( c => ( c.name === currentGame.course.name && c.layout === currentGame.course.layout) )
+                if (!courseStats) {
+                    courseStats = { name: currentGame.course.name, layout: currentGame.course.layout, games: 0, players: [] }
+                    courses.push( courseStats )
+                }
+
                 games.push(currentGame)
                 for (let playerObj of currentGame.players) {
-                    console.log(playerObj.name + " @ " + currentGame.course.name + " = " + playerObj.plusminus)
-                    if (!(playerObj.name in courses[currentGame.course.name].players)) {
-                        courses[currentGame.course.name].players[playerObj.name] = { runningHC: 0, HC: 0, games: 0 }
+                   
+                    let coursesPlayerObj = courseStats.players.find(p => p.name === playerObj.name)
+                    if (!coursesPlayerObj) {
+                        coursesPlayerObj = { name: playerObj.name, runningHC: 0, HC: 0, games: 0 }
+                        courseStats.players.push(coursesPlayerObj)
                     }
-                    const coursesPlayerObj = courses[currentGame.course.name].players[playerObj.name]
 
                     playerObj['HC'] = coursesPlayerObj.HC || 0
                     playerObj['totalHC'] = Number(playerObj['total']) - playerObj.HC
@@ -60,9 +58,9 @@ const parse = async () => {
         currentGame.players.push({ name, score, total, plusminus })
 
     }
-    //console.log( util.inspect(courses, true, null, true) )
-    console.log( util.inspect(games, true, null, true))
-    return games
+    console.log( util.inspect(courses, true, null, true) )
+    //console.log( util.inspect(games, true, null, true))
+    return { games, courses }
 }
 
 module.exports = parse;
