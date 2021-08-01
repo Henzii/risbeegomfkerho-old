b0ab563281bc99ccca03ee7baf80067838559735
Henzii <80477931+Henzii@users.noreply.github.com>
2021-08-01 15:38:40 +0300

    bug

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
-const parse = require('./testi');
 const games = require('./games.json')
 app.use(cors())
 
